89faed40230b5cd50348ff084f283781d3f9467e
Gigel <cristian.lungeanu@stud.acs.upb.ro>
2021-02-27 04:32:15 +0200

    Update lazyloader.js

--- js/lazyloader.js
+++ js/lazyloader.js
@@ -1,41 +1,41 @@
 
 (function () {
 
-var queue = [
-	{ lib: 'jquery-ui', url: 'js/libs/jquery-ui.min.js' },
-	{ lib: 'smoothScroll', url: 'js/util/smoothScroll.js' },
-	{ lib: 'jfeed', url: 'js/util/jquery.jfeed.js' },
-	{ lib: 'lightbox', url: 'js/util/jquery.lightbox_me.js' },
-	{ lib: 'colorPicker', url: 'js/util/colorPicker/js/colorpicker.js' }
-];
+	var queue = [
+		{ lib: 'jquery-ui', url: 'js/libs/jquery-ui.min.js' },
+		{ lib: 'smoothScroll', url: 'js/util/smoothScroll.js' },
+		{ lib: 'jfeed', url: 'js/util/jquery.jfeed.js' },
+		{ lib: 'lightbox', url: 'js/util/jquery.lightbox_me.js' },
+		{ lib: 'colorPicker', url: 'js/util/colorPicker/js/colorpicker.js' }
+	];
 
-var libs = {};
+	var libs = {};
 
-function addDependencyFunction(lib, fn) {
-	if (libs[lib].loaded)
-		fn();
-	else
-		libs[lib].fn.push(fn);
-}
+	function addDependencyFunction(lib, fn) {
+		if (libs[lib].loaded)
+			fn();
+		else
+			libs[lib].fn.push(fn);
+	}
 
-function runLibFunctions(lib) {
-	libs[lib].fn.forEach(function (fn) {
-		fn();
+	function runLibFunctions(lib) {
+		libs[lib].fn.forEach(function (fn) {
+			fn();
+		});
+	}
+
+	queue.forEach(function (item) {
+		var lib = item.lib;
+		var script = document.createElement('script');
+		libs[lib] = { loaded: false, fn: [] };
+		script.onload = function () {
+			libs[lib].loaded = true;
+			runLibFunctions(lib);
+		};
+		script.src = item.url;
+		document.head.appendChild(script);
 	});
-}
 
-queue.forEach(function (item) {
-	var lib = item.lib;
-	var script = document.createElement('script');
-	libs[lib] = { loaded: false, fn: [] };
-	script.onload = function () {
-		libs[lib].loaded = true;
-		runLibFunctions(lib);
-	};
-	script.src = item.url;
-	document.head.appendChild(script);
-});
-
-window.addDependencyFunction = addDependencyFunction;
+	window.addDependencyFunction = addDependencyFunction;
 
 })();
